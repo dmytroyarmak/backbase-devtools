10be7ca7c215de02375832783bc56bcdfb471e33
Dmytros <dmytros@backbase.com>
2016-06-24 14:17:54 +0300

    added functionality for right panel

--- devtools.js
+++ devtools.js
@@ -5,7 +5,44 @@ chrome.devtools.panels.create('Backbase', 'backbase.png', 'panel.html', function
 });
 
 chrome.devtools.panels.elements.createSidebarPane('Backbase Item Model', function(sidebar) {
-    sidebar.setObject({
-        lorem: 'ipsum'
-    });
+
+    var getInfo = function (){
+       // var elm = b$.portal.portalModel.all[$0.attributes['data-pid'].value];
+        var propertyList = ['Description','TemplateName','templateUrl','title','version'];
+        var dataList = ['contextItemName','extendedItemName','name','securityProfile'];
+        var elm, pref;
+        var obj = {};
+        var errorMSg = {massage : 'This element do not have a widget content'};
+        obj.preference = {};
+
+        if ($0.closest("[data-pid]") == null) {
+            errorMSg;
+        } else {
+
+            elm = b$.portal.portalModel.all[$0.closest("[data-pid]").attributes['data-pid'].value];
+            pref = elm.attributes;
+            
+            // get preference
+            for (key in pref) {
+                if (propertyList.indexOf(pref[key].localName) > -1 ){
+                    obj.preference[pref[key].localName] = pref[key].nodeValue;
+                }
+            }
+            //get other information
+            for (key in elm) {
+                if (dataList.indexOf(key) > -1 ){
+                    obj[key] = elm[key];
+                }
+            }
+            obj;
+        }
+    };
+
+    function update() {
+        sidebar.setExpression(getInfo.toString().match(/function[^{]+\{([\s\S]*)\}$/)[1]);
+    }
+    update();
+
+    chrome.devtools.panels.elements.onSelectionChanged.addListener(update);
+
 });
