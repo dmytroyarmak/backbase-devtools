f2902fef28a57c0602aff1a1dac2c3ea05c3c0f4
Dmytro Yarmak <dmytroy@backbase.com>
2016-06-24 10:37:01 +0300

    Add dummy sidebar pane for elements panel

--- devtools.js
+++ devtools.js
@@ -3,3 +3,9 @@ console.log('devtools.js');
 chrome.devtools.panels.create('Backbase', 'backbase.png', 'panel.html', function(panel) {
     console.log('Panel created', panel);
 });
+
+chrome.devtools.panels.elements.createSidebarPane('Backbase Item Model', function(sidebar) {
+    sidebar.setObject({
+        lorem: 'ipsum'
+    });
+});
